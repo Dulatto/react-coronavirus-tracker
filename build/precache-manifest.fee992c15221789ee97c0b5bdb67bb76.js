7f954e504d87fee13dfc8dbbc33f36d5ad3b3180
Dulatto <dulat.omarov@gmail.com>
2021-02-03 23:54:48 -0500

    package

---
+++ build/precache-manifest.fee992c15221789ee97c0b5bdb67bb76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ca04fce677c270fad59ecdaaa79df7",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "5a16e097f01e6d01f403",
+    "url": "/coronatracker/static/css/main.002f06f9.chunk.css"
+  },
+  {
+    "revision": "f837b2166df0a22e99fc",
+    "url": "/coronatracker/static/js/2.d8da6ef2.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/coronatracker/static/js/2.d8da6ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a16e097f01e6d01f403",
+    "url": "/coronatracker/static/js/main.2b3a618b.chunk.js"
+  },
+  {
+    "revision": "55eb73d4fba70127f3c0",
+    "url": "/coronatracker/static/js/runtime-main.7256f8e2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/logo.d7265326.png"
+  }
+]);

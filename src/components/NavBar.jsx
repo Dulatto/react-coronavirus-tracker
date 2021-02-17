1ca662b5d21f34eaacd6aeaf0b40a4d2828271de
Dulatto <dulat.omarov@gmail.com>
2021-02-16 22:49:31 -0500

    style adjust

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
-    <nav className='navbar navbar-light bg-light'>
+    <nav className='navbar navbar-light bg-light border-bottom'>
       <div className='container'>
         <a className='navbar-brand' href='/'>
           <img src={logo} alt='Corona Logo' />
